fb1b31e44efc119d6e9d9c0115846ad1790df92c
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-04-11 21:25:58 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
+<<<<<<< HEAD
 console.log('config.js');
 console.log('version1');
+console.log('version2');
+=======
+console.log('config.js');
+console.log('version1');
+>>>>>>> 423380a951beb89e1a3f69e4fdd4860f62dbf1f9
